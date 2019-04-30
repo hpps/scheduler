f9392179887850a2117aea2ff1ef05d578d513dc
Greg Cross <grog@uic.edu>
2019-04-30 18:48:35 -0500

    Add files via upload
    Adding DMPS support

--- src/scheduler.c
+++ src/scheduler.c
@@ -7,7 +7,7 @@
 extern long long int CYCLE_VAL;
 
 // these are all tunables
-#define QUANTUM_SIZE 100000
+#define QUANTUM_SIZE 1000000
 #define EPOCH_SIZE 5000
 #define MOPL 0.3      // MOPL * PLNUM = 1 (ideally)
 #define PLNUM 3       // we will try 4 as well
@@ -18,10 +18,11 @@ extern long long int CYCLE_VAL;
 #include <math.h>
 #include <unistd.h>
 #include "params.h"
+#include <limits.h>
 
 #define MAX_NUMCORES 16
 
-int Ncore = MAX_NUMCORES;
+//int Ncore = NUMCORES;
 //int Nchannel = NUM_CHANNELS;
 
 int quantum_counter;
@@ -34,6 +35,8 @@ int ReqCnt[MAX_NUMCORES];
 int EReqCnt[MAX_NUMCORES][MAX_NUM_CHANNELS];
 int QReqCnt[MAX_NUMCORES][MAX_NUM_CHANNELS];
 
+int row_hit[MAX_NUMCORES][MAX_NUM_CHANNELS];
+
 int NxtGroup[MAX_NUMCORES]; 
 int PreGroup[MAX_NUMCORES];
 int CurGroup[MAX_NUMCORES];
@@ -41,14 +44,17 @@ int CurGroup[MAX_NUMCORES];
 int ReqPL;
 int GroupTh;
 
+long long last_quantum = -1000000;
+long long last_epoch = -5000;
+
 void init_scheduler_vars()
 {
-	// initialize all scheduler variables here
+    // initialize all scheduler variables here
 
         quantum_counter = 0;
         epoch_counter = 0;
 
-	return;
+    return;
 }
 
 // write queue high water mark; begin draining writes if write queue exceeds this value
@@ -86,17 +92,19 @@ void grouping_algorithm() {
 
     TotalReqCnt = 0;
 
-    for(i = 0; i < Ncore; i++) {
+    for(i = 0; i < NUMCORES; i++) {
         ReqCnt[i] = 0;
         for(j = 0; j < NUM_CHANNELS; j++) {
             ReqCnt[i] += QReqCnt[i][j];
+//            printf("QReqCnt[%d][%d] = %d; ", i, j, QReqCnt[i][j]);
         }
         TotalReqCnt += ReqCnt[i];
+//        printf("\n");
     }
 
-    GroupTh = (TotalReqCnt * MOPL)/Ncore;
+    GroupTh = (TotalReqCnt * MOPL)/NUMCORES;
 
-    for(i = 0; i < Ncore; i++) {
+    for(i = 0; i < NUMCORES; i++) {
         if (ReqCnt[i] < GroupTh) {
             CurGroup[i] = 0; 
         }
@@ -106,19 +114,10 @@ void grouping_algorithm() {
         NxtGroup[i] = PreGroup[i] && CurGroup[i];
         PreGroup[i] = CurGroup[i];
     }
-    ReqPL = TotalReqCnt * MOPL * EPOCH_SIZE / QUANTUM_SIZE / Ncore / NUM_CHANNELS;
-
-}
-
-void prioritization_algorithm()
-{
-    //(1) Highest level first: Requests from applications at higher priority levels are prioritized.
-    //—The priority level of latency-sensitive applications is higher than that of bandwidth-sensitive applications by one level initially.
-    //—The priority of an application is lowered by one level when the number of served read requests is over predetermined thresholds.
-
-    //(2) Row-hit first: Row-hit requests are prioritized over row-closed and row-conflict requests.
-
-    //(3) Oldest first: Older requests are prioritized over younger requests.
+//    ReqPL = TotalReqCnt * MOPL * EPOCH_SIZE / QUANTUM_SIZE / Ncore / NUM_CHANNELS;
+    ReqPL = (int)((float)TotalReqCnt*MOPL*(float)EPOCH_SIZE/(float)QUANTUM_SIZE/(float)NUMCORES/(float)NUM_CHANNELS);
+    printf("ReqPL = %d = %d * %f *  %d / %d / %d / %d\n\n", ReqPL, TotalReqCnt, MOPL, EPOCH_SIZE, QUANTUM_SIZE, NUMCORES, NUM_CHANNELS);
+//    sleep(1);
 
 }
 
@@ -126,100 +125,218 @@ void schedule(int channel)
 {
 
     int i;
-    int j = channel;
+    int j;
 
-    request_t * curr = NULL;
+    request_t * rd_ptr = NULL;
+    request_t * wr_ptr = NULL;
 
-    if (quantum_counter == 0) {
+    request_t * rd_next = NULL;
 
-        // Initialization at the beginning of every quantum
-        for(i = 0; i < Ncore; i++) {
-            QReqCnt[i][j] = 0;
-            EReqCnt[i][j] = 0;
-            IniPri[i][j] = PLNUM - NxtGroup[i];
+    int highest_pri;
+    int row_hit_found;
+
+    long long int earliest_time;
+
+    // Cleared every epoch
+    if(CYCLE_VAL - last_epoch >= EPOCH_SIZE) {
+        printf("%lld\n", CYCLE_VAL);
+
+        for(j = 0; j < NUM_CHANNELS; j++) {
+//            printf("EReqCnt[j=%d] = ", j);
+            printf("DynPri[j=%d] = ", j);
+            for(i = 0; i < NUMCORES; i++) {
+//                printf("%d ", EReqCnt[i][j]);
+                printf("%d/%d ", DynPri[i][j],row_hit[i][j]);
+                row_hit[i][j] = 0;
+                EReqCnt[i][j] = 0;
+                DynPri[i][j] = IniPri[i][j];
+            }
+            printf("\n");
         }
+        printf("\n");
+
+//        if (j == 0) {
+            last_epoch = floor(CYCLE_VAL/EPOCH_SIZE)*EPOCH_SIZE;
+//        }
     }
+    // Cleared every quantum
+    if (CYCLE_VAL - last_quantum >= QUANTUM_SIZE) {
 
+        grouping_algorithm();
+        for(j = 0; j < NUM_CHANNELS; j++) {
+            printf("QReqCnt[j=%d] = ", j);
+            for(i = 0; i < NUMCORES; i++) {
+                printf("%d ", QReqCnt[i][j]);
+                QReqCnt[i][j] = 0;
+                IniPri[i][j] = PLNUM - NxtGroup[i];
+            }
+            printf("\n");
+        }
+        printf("\n");
+//        sleep(1);
 
-    if (channel == 0) {
-        quantum_counter++;
-        epoch_counter++;
+//        if (j == 0) {
+            last_quantum = floor(CYCLE_VAL/QUANTUM_SIZE)*QUANTUM_SIZE;
+//        }
+        // Initialization at the beginning of every quantum
     }
+//    sleep(1);
 
-//    printf("channel = %d, quantum = %d, epoch = %d\n", j, quantum_counter, epoch_counter);
 
+
+
+     
+
+/*
     //counting
     for(int j=0; j<NUM_CHANNELS; j++){
-        LL_FOREACH(read_queue_head[j],curr){
+        LL_FOREACH(read_queue_head[j],rd_ptr){
             for(int i=0; i<NUMCORES; i++){
-                printf("%d.%d: thread_id = %d,", i, j, curr->thread_id);
-                printf("request_served = %d\n", curr->request_served);
+                printf("%d.%d: thread_id = %d,", i, j, rd_ptr->thread_id);
+                printf("request_served = %d\n", rd_ptr->request_served);
             }
         }
     }
 
+*/
 
     // Memory Occupancy Monitor
-    for(i = 0; i < Ncore; i++) {
-        LL_FOREACH(read_queue_head[channel], curr) {
-//            printf("channel %d: read_queue_head[%d] = %x\n", channel, channel, read_queue_head[channel]);
+//    for(i = 0; i < Ncore; i++) {
+
+//        int k = 0;
+        LL_FOREACH(read_queue_head[channel], rd_ptr) {
+//            printf("CV=%d, l_e=%lld, l_q=%lld, ch %d: %x @ %lld\n", CYCLE_VAL, last_epoch, last_quantum, channel, rd_ptr, rd_ptr->arrival_time);
+//            printf("k = %d, rd_ptr->thread_id = %d\n", k++, rd_ptr->thread_id);
+//            printf("channel %d: read_queue_head[%d] = %0x\n", channel, channel, read_queue_head[channel]);
             if (read_queue_head[channel]->request_served == TRUE) {
-//                printf("TRUE\n");
-                QReqCnt[i][j]++;
-                EReqCnt[i][j]++;
+ //               printf("TRUE\n");
+                QReqCnt[rd_ptr->thread_id][channel]++;
+                EReqCnt[rd_ptr->thread_id][channel]++;
             }
             else {
 //                printf("FALSE\n");
             }
+
+            if (rd_ptr->next_command == COL_READ_CMD) {
+                row_hit[rd_ptr->thread_id][channel] = 1;
+            }
         }
-    }
+//        printf("---\n", channel, rd_ptr);
+//        sleep(1);
+ //   }
 
     // Multi-Level Comparator
-    for(i = 0; i < Ncore; i++) {
-        if (EReqCnt[i][j] < ReqPL) {
-            DynPri[i][j] = IniPri[i][j]; // Dynamic priority
+    for(i = 0; i < NUMCORES; i++) {
+        if (EReqCnt[i][channel] < ReqPL) {
+            DynPri[i][channel] = IniPri[i][channel]; // Dynamic priority
         }
-        else if (EReqCnt[i][j] >= (PLNUM-1)*ReqPL) {
-            DynPri[i][j] = 1;
+        else if (EReqCnt[i][channel] >= (PLNUM-1)*ReqPL) {
+            DynPri[i][channel] = 1;
         }
         else {
-            DynPri[i][j] = PLNUM - floor(EReqCnt[i][j]/ReqPL);
+            DynPri[i][channel] = PLNUM - floor(EReqCnt[i][channel]/ReqPL);
         }
     }
 
-    // Cleared every epoch
-    if(epoch_counter == EPOCH_SIZE) {
 
-        prioritization_algorithm();
-
-        for(i = 0; i < Ncore; i++) {
-            EReqCnt[i][j] = 0;
-            DynPri[i][j] = IniPri[i][j];
-        }
-
-        epoch_counter = 0;
+    // ALGORITHM 3: PRIORITIZATION
+    
+    // if in write drain mode, keep draining writes until the
+    // write queue occupancy drops to LO_WM
+    if (drain_writes[channel] && (write_queue_length[channel] > LO_WM)) {
+      drain_writes[channel] = 1; // Keep draining.
+    }
+    else {
+      drain_writes[channel] = 0; // No need to drain.
     }
 
-     
-//    printf("channel = %d\n", j);
-    if (quantum_counter == QUANTUM_SIZE) {
-
-        printf("j = %d, NUM_CHANNELS-1 = %d\n", j, NUM_CHANNELS-1);
-        for(i = 0; i < Ncore; i++) {
-            printf("QReqCnt[%d][%d] = %d; ", i, j, QReqCnt[i][j]);
-            printf("EReqCnt[%d][%d] = %d\n", i, j, EReqCnt[i][j]);
-
-        }
-
-        sleep(10);
-
-        if (j == NUM_CHANNELS-1) {
-            grouping_algorithm();
-            quantum_counter = 0;
-        }
-
+    // initiate write drain if either the write queue occupancy
+    // has reached the HI_WM , OR, if there are no pending read
+    // requests
+    if(write_queue_length[channel] > HI_WM)
+    {
+        drain_writes[channel] = 1;
+    }
+    else {
+      if (!read_queue_length[channel])
+        drain_writes[channel] = 1;
     }
 
+
+    // If in write drain mode, look through all the write queue
+    // elements (already arranged in the order of arrival), and
+    // issue the command for the first request that is ready
+    if(drain_writes[channel])
+    {
+
+        LL_FOREACH(write_queue_head[channel], wr_ptr)
+        {
+            if(wr_ptr->command_issuable)
+            {
+                issue_request_command(wr_ptr);
+                break;
+            }
+        }
+        return;
+    }
+
+    // Draining Reads
+    // look through the queue and find the first request whose
+    // command can be issued in this cycle and issue it 
+    // Simple FCFS 
+    if(!drain_writes[channel])
+    {
+        rd_next = NULL;
+        earliest_time = LLONG_MAX;
+        highest_pri = 0;
+        row_hit_found = 0;
+
+        LL_FOREACH(read_queue_head[channel],rd_ptr)
+        {
+            if(rd_ptr->command_issuable)
+            {
+                if(DynPri[rd_ptr->thread_id][channel] > highest_pri)
+                {
+                    highest_pri = DynPri[rd_ptr->thread_id][channel];
+                    rd_next = rd_ptr;
+                    earliest_time = rd_ptr->arrival_time;
+                    row_hit_found = 0;
+//                    printf("thread %d got priority\n", rd_ptr->thread_id);
+                }
+                if(row_hit[rd_ptr->thread_id][channel] == 1)
+                {
+                    row_hit_found = 1;
+                    if(rd_ptr->arrival_time < earliest_time)    
+                    {
+                        rd_next = rd_ptr;
+                        earliest_time = rd_ptr->arrival_time;
+//                        printf("thread %d got priority\n", rd_ptr->thread_id);
+                    }
+                }
+                else if(row_hit_found == 0)
+                {
+                    if(rd_ptr->arrival_time < earliest_time)    
+                    {
+                        rd_next = rd_ptr;
+                        earliest_time = rd_ptr->arrival_time;
+//                        printf("thread %d got priority\n", rd_ptr->thread_id);
+                    }
+                }
+//                printf("      thr i=%d, ch j=%d: pri=%d, row_hit=%d, time=%lld\n", rd_ptr->thread_id, channel, DynPri[rd_ptr->thread_id][channel], row_hit[rd_ptr->thread_id][channel], rd_ptr->arrival_time);
+            }
+        }
+
+        if(rd_next != NULL)
+        {
+//            printf("NEXT: thr i=%d,\tch j=%d:\tpri=%d,\trow_hit=%d,\ttime=%lld\n", rd_next->thread_id, channel, DynPri[rd_next->thread_id][channel], row_hit[rd_next->thread_id][channel], rd_next->arrival_time);
+            issue_request_command(rd_next);
+        }
+    }
+
+//    printf("channel = %d, quantum = %d, epoch = %d\n", j, quantum_counter, epoch_counter);
+
+
+    return;
 }
 
 void scheduler_stats()
